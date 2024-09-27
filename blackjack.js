47f6414f7fd4f6911c05743e77845f1da82f3e3f
Hminl <milksing@naver.com>
2024-09-27 15:19:43 +0900

    blackjack&iceCream
    sfdfsf

---
+++ blackjack.js
@@ -0,0 +1,29 @@
+// blackjack.js
+let card = [11, 9, 1];
+let cardBank = [9, 7, 1, 4];
+
+let sum = card[0] + card[1];
+let bankSum = cardBank[0] + cardBank[1];
+
+if (sum == 21) {
+    console.log('You BlackJack');
+} else {
+    sum += card[2];
+}
+
+let i = 2;
+while (bankSum > 17) {
+    bankSum += cardBank[i];
+    i++;
+}
+
+if (bankSum > 21 || (sum <= 21 && sum > bankSum)) {
+ console.log('You win');
+} if (sum > 21 || (bankSum <= 21 && sum < bankSum)){
+ console.log('Bank wins');
+} if (bankSum == sum ){
+    console.log('Draw');
+}
+console.log(`You have ${sum} points`);
+console.log(`Bank have ${bankSum} points`);
+
